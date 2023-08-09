7dcd4487e019cda7e0439efb87ac01d3e4b4a90e
CatianoNorberto <catianonorberto@gmail.com>
2023-08-09 16:12:02 -0300

    Configurando o knex para banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,18 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { knex } from './database'
 
 const server = fastify()
 
-server.get('/hello', () => {
-  return 'Hello node'
+server.get('/hello', async () => {
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
-server.listen({
-  port: 3333,
-}).then(() => {
-  console.log('HTTP server Running on port 3333')
-})
+server
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server Running on port 3333')
+  })
